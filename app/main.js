9a879d3ee19efd6c3117ee0b1d28739c9fe83f23
Mara K <mariia.kucheieva@gmail.com>
2024-04-19 14:11:55 +0300

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -5,11 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.get("data", (req,res) => {
-    res.send('HTTP/1.1 200 OK\r\n\r\n')
-    socket.end();
+  socket.on("data", () => {
+    const response = "HTTP/1.1 200 OK\r\n\r\n";
 
-    server.close();
+    socket.write(response);
   });
 });
 
