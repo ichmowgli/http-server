01d8264c926741065d75342fda745f9d8d68de80
Mara K <mariia.kucheieva@gmail.com>
2024-04-19 23:26:13 +0300

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,36 +1,41 @@
 const net = require("net");
-
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const startLine = data.toString().split("\r\n");
-    const [, path] = startLine[0].split(" ");
-    if (path[1] === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (requestPath[1].includes("/echo/")) {
-      const string = path.split("/echo/")[1];
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${string.length}\r\n\r\n${string}`
-      );
-    } else if (requestPath[1].includes("/user-agent")) {
-      let userAgent = "";
-      for (let i = 1; i < startLine.length; i++) {
-        if (startLine[i].startsWith("User-Agent:")) {
-          userAgent = startLine[i].substring(12);
-          break;
-        }
-      }
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-      1;
-      socket.write(response);
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    const startLine = data.toString().split("\r\n")[0];
+    const [, path] = startLine.split(" ");
+    if (path === "/") {
+      socket.end("HTTP/1.1 200 OK\r\n\r\n");
+      return;
     }
-    socket.end();
+    if (path.startsWith("/echo/")) {
+      const string = path.split("/echo/")[1];
+      socket.end(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${Buffer.byteLength(
+          string,
+          ["utf-8"]
+        )}\r\n\r\n${string}\r\n\r\n`
+      );
+      return;
+    }
+    if (path.startsWith("/user-agent")) {
+      const lines = data.toString().split("\r\n");
+      const userAgentIndex = lines.findIndex((line) =>
+        line.startsWith("User-Agent:")
+      );
+      const userAgent = lines[userAgentIndex].split(": ")[1];
+      socket.end(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${Buffer.byteLength(
+          userAgent,
+          ["utf-8"]
+        )}\r\n\r\n${userAgent}\r\n\r\n`
+      );
+      return;
+    }
+
+    socket.end("HTTP/1.1 404 Not Found\r\n\r\n");
   });
 });
-
 server.listen(4221, "localhost");
