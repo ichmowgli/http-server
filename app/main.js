5b99566f7083024e057741c1ffb53c2eb8022621
Mara K <mariia.kucheieva@gmail.com>
2024-04-19 15:37:36 +0300

    pass stage

--- app/main.js
+++ app/main.js
@@ -5,10 +5,15 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    const response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    socket.write(response);
+  socket.on("data", (data) => {
+    const request = data.toString().split("\r\n");
+    const [, path] = request[0].split(" ");
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+    socket.end();
   });
 });
 
